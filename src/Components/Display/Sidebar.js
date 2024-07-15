3c93309c513f03a3809eba59d7fe0f8184a61033
leanneholmes <lholmes24@my.bcit.ca>
2024-07-15 13:39:02 -0700

    Days per page implemented

--- src/Components/Display/Sidebar.js
+++ src/Components/Display/Sidebar.js
@@ -1,107 +1,139 @@
-import React from 'react';
+import React from "react";
 
 // Sidebar Component imports
-import CMYKConverter from './SidebarComponents/CMYKConverter';
-import FontSettings from './SidebarComponents/FontSettings';
-import GridLineSettings from './SidebarComponents/GridLineSettings';
-import ColorSettings from './SidebarComponents/ColorSettings';
+import CMYKConverter from "./SidebarComponents/CMYKConverter";
+import FontSettings from "./SidebarComponents/FontSettings";
+import GridLineSettings from "./SidebarComponents/GridLineSettings";
+import ColorSettings from "./SidebarComponents/ColorSettings";
+import DaysPerPageSettings from "./SidebarComponents/DaysPerPageSettings";
 
 // CSS import
-import '../../css/Sidebar.css';
+import "../../css/Sidebar.css";
 
 // External import
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MaterialIcon from 'material-icons-react';
-
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MaterialIcon from "material-icons-react";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <h3>Customization Menu</h3>
-            <div className='sidebarItems'>
-                <div className='sidebarBlock'>
-                    <Accordion disableGutters elevation={0}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon color="primary" />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <span className='sidebarHeader'>
-                                <span className="menu-icon">
-                                    <MaterialIcon icon="autorenew" color='#5e72e4' size={18} />
-                                </span>CMYK Converter</span>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <CMYKConverter />
-                        </AccordionDetails>
-                    </Accordion>
-                </div>
-                <div className="lineBreak"></div>
-                <div className='sidebarBlock'>
-                    <Accordion disableGutters elevation={0}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon color="primary" />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <span className='sidebarHeader'>
-                                <span className="menu-icon">
-                                    <MaterialIcon icon="format_color_fill" invert color='#11cdef' size={18} />
-                                </span>Color Settings</span>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <ColorSettings />
-                        </AccordionDetails>
-                    </Accordion>
-                </div>
-                <div className="lineBreak"></div>
-                <div className='sidebarBlock'>
-                    <Accordion disableGutters elevation={0}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon color="primary" />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <span className='sidebarHeader'>
-                                <span className="menu-icon">
-                                    <MaterialIcon icon="text_fields" color='#fb7253' size={18} />
-                                </span>Font Settings</span>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <FontSettings />
-                        </AccordionDetails>
-                    </Accordion>
-                </div>
-                <div className="lineBreak"></div>
-                <div className='sidebarBlock'>
-                    <Accordion disableGutters elevation={0}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon color="primary" />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <span className='sidebarHeader'>
-                                <span className="menu-icon">
-                                    <MaterialIcon icon="view_kanban" color='#f5365c' size={18} />
-                                </span>Grid Line Settings</span>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <GridLineSettings />
-                        </AccordionDetails>
-                    </Accordion>
-                </div>
-                <div className="lineBreak"></div>
-                {/* Checkbox to clear empty rows  */}
-                {/* <FormGroup sx={{ pl: 0.2, pt: 0.5 }}>
-                    <FormControlLabel control={<Checkbox />} label="Clear Empty Rows" />
-                </FormGroup> */}
-            </div>
+  return (
+    <div className="sidebar">
+      <h3>Customization Menu</h3>
+      <div className="sidebarItems">
+        <div className="sidebarBlock">
+          <Accordion disableGutters elevation={0}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon color="primary" />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <span className="sidebarHeader">
+                <span className="menu-icon">
+                  <MaterialIcon icon="autorenew" color="#5e72e4" size={18} />
+                </span>
+                CMYK Converter
+              </span>
+            </AccordionSummary>
+            <AccordionDetails>
+              <CMYKConverter />
+            </AccordionDetails>
+          </Accordion>
         </div>
-    );
+        <div className="lineBreak"></div>
+        <div className="sidebarBlock">
+          <Accordion disableGutters elevation={0}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon color="primary" />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <span className="sidebarHeader">
+                <span className="menu-icon">
+                  <MaterialIcon
+                    icon="format_color_fill"
+                    invert
+                    color="#11cdef"
+                    size={18}
+                  />
+                </span>
+                Color Settings
+              </span>
+            </AccordionSummary>
+            <AccordionDetails>
+              <ColorSettings />
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="lineBreak"></div>
+        <div className="sidebarBlock">
+          <Accordion disableGutters elevation={0}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon color="primary" />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <span className="sidebarHeader">
+                <span className="menu-icon">
+                  <MaterialIcon icon="text_fields" color="#fb7253" size={18} />
+                </span>
+                Font Settings
+              </span>
+            </AccordionSummary>
+            <AccordionDetails>
+              <FontSettings />
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="lineBreak"></div>
+        <div className="sidebarBlock">
+          <Accordion disableGutters elevation={0}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon color="primary" />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <span className="sidebarHeader">
+                <span className="menu-icon">
+                  <MaterialIcon icon="view_kanban" color="#f5365c" size={18} />
+                </span>
+                Grid Line Settings
+              </span>
+            </AccordionSummary>
+            <AccordionDetails>
+              <GridLineSettings />
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="lineBreak"></div>
+        <div className="sidebarBlock">
+          <Accordion disableGutters elevation={0} defaultExpanded={true}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon color="primary" />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <span className="sidebarHeader">
+                <span className="menu-icon">
+                  <MaterialIcon
+                    icon="calendar_month"
+                    color="#2fa84f"
+                    size={18}
+                  />
+                </span>
+                Days per Page
+              </span>
+            </AccordionSummary>
+            <AccordionDetails>
+              <DaysPerPageSettings />
+            </AccordionDetails>
+          </Accordion>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/Context/ScheduleContext/ScheduleReducer.js
+++ src/Context/ScheduleContext/ScheduleReducer.js
@@ -1,112 +1,121 @@
 import {
-    PARSE_GRIDSCREENTIMES,
-    MAP_VENUENAME,
-    SET_DATE,
-    SET_COLOR,
-    SET_FONT,
-    SET_GRIDLINE,
-    SET_CUSTOM_ID,
-    CLOSE_CUSTOM,
-    CHANGE_SCREEN,
-} from '../ActionType';
+  PARSE_GRIDSCREENTIMES,
+  MAP_VENUENAME,
+  SET_DATE,
+  SET_COLOR,
+  SET_FONT,
+  SET_GRIDLINE,
+  SET_DAYS_PER_PAGE,
+  SET_CUSTOM_ID,
+  CLOSE_CUSTOM,
+  CHANGE_SCREEN,
+} from "../ActionType";
 
 export default (state, action) => {
-    switch (action.type) {
-        case MAP_VENUENAME:
-        case PARSE_GRIDSCREENTIMES:
-
+  switch (action.type) {
+    case MAP_VENUENAME:
+    case PARSE_GRIDSCREENTIMES:
+      return {
+        ...state,
+        gridScreenTimes: action.screenTimes,
+      };
+    case SET_DATE:
+      return {
+        ...state,
+        gridScreenTimes: state.gridScreenTimes.map((t) => {
+          if (t.id == action.index) {
             return {
-                ...state,
-                gridScreenTimes: action.screenTimes,
+              ...t,
+              date: "test",
             };
-        case SET_DATE:
+          } else {
+            return t;
+          }
+        }),
+      };
+    case SET_COLOR:
+      return {
+        ...state,
+        colorSettings: { ...state.colorSettings, [action.id]: action.color },
+      };
+    case SET_FONT:
+      return {
+        ...state,
+        fontSettings: {
+          ...state.fontSettings,
+          [action.settingID]: action.settingVal,
+        },
+      };
+    case SET_GRIDLINE:
+      return {
+        ...state,
+        gridLineSettings: {
+          ...state.gridLineSettings,
+          [action.settingID]: action.settingVal,
+        },
+      };
+    case SET_DAYS_PER_PAGE:
+      return {
+        ...state,
+        daysPerPage: action.value,
+      };
+    case SET_CUSTOM_ID:
+      return {
+        ...state,
+        customBarTarget: {
+          dateID: action.id.dateID,
+          venueID: action.id.venueID,
+          screenID: action.id.screenID,
+          display: true,
+        },
+      };
+    case CLOSE_CUSTOM:
+      return {
+        ...state,
+        customBarTarget: {
+          ...state.customBarTarget,
+          display: false,
+        },
+      };
+    case CHANGE_SCREEN:
+      return {
+        ...state,
+        gridScreenTimes: state.gridScreenTimes.map((d) => {
+          if (d.id == action.id.dateID) {
             return {
-                ...state,
-                gridScreenTimes: state.gridScreenTimes.map((t) => {
-                    if (t.id == action.index) {
+              ...d,
+              venue: d.venue.map((v) => {
+                if (v.id == action.id.venueID) {
+                  return {
+                    ...v,
+                    screens: v.screens.map((s) => {
+                      if (s.id == action.id.screenID) {
                         return {
-                            ...t,
-                            date: 'test',
+                          ...s,
+                          customized: true,
+                          duration: action.screen.duration,
+                          filmTitle: action.screen.filmTitle,
+                          pageLocation: action.screen.pageLocation,
+                          startTime: action.screen.startTime,
+                          filmTitleText: action.screen.filmTitleText,
+                          filmDetailsText: action.screen.filmDetailsText,
                         };
-                    } else {
-                        return t;
-                    }
-                }),
+                      } else {
+                        return s;
+                      }
+                    }),
+                  };
+                } else {
+                  return v;
+                }
+              }),
             };
-        case SET_COLOR:
-            return {
-                ...state,
-                colorSettings: { ...state.colorSettings, [action.id]: action.color },
-            };
-        case SET_FONT:
-            return {
-                ...state,
-                fontSettings: { ...state.fontSettings, [action.settingID]: action.settingVal },
-            };
-        case SET_GRIDLINE:
-            return {
-                ...state,
-                gridLineSettings: { ...state.gridLineSettings, [action.settingID]: action.settingVal },
-            };
-        case SET_CUSTOM_ID:
-            return {
-                ...state,
-                customBarTarget: {
-                    dateID: action.id.dateID,
-                    venueID: action.id.venueID,
-                    screenID: action.id.screenID,
-                    display: true,
-                },
-            };
-        case CLOSE_CUSTOM:
-            return {
-                ...state,
-                customBarTarget: {
-                    ...state.customBarTarget,
-                    display: false,
-                },
-            };
-        case CHANGE_SCREEN:
-            return {
-                ...state,
-                gridScreenTimes: state.gridScreenTimes.map((d) => {
-                    if (d.id == action.id.dateID) {
-                        return (
-                            {
-                                ...d,
-                                venue: d.venue.map((v) => {
-                                    if (v.id == action.id.venueID) {
-                                        return ({
-                                            ...v,
-                                            screens: v.screens.map((s) => {
-                                                if (s.id == action.id.screenID) {
-                                                    return ({
-                                                        ...s,
-                                                        customized: true,
-                                                        duration: action.screen.duration,
-                                                        filmTitle: action.screen.filmTitle,
-                                                        pageLocation: action.screen.pageLocation,
-                                                        startTime: action.screen.startTime,
-                                                        filmTitleText: action.screen.filmTitleText,
-                                                        filmDetailsText: action.screen.filmDetailsText,
-                                                    });
-                                                } else {
-                                                    return s;
-                                                }
-                                            }),
-                                        });
-                                    } else {
-                                        return v;
-                                    }
-                                }),
-                            }
-                        );
-                    } else {
-                        return d;
-                    }
-                }),
-            };
-        default:
-            return state;
-    }
+          } else {
+            return d;
+          }
+        }),
+      };
+    default:
+      return state;
+  }
 };
